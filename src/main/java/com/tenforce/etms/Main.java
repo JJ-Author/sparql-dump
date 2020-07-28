34cdbc46f56a03b6f7eb97e2c37a49cd8053840b
JJ-Author <JJ-Author@users.noreply.github.com>
2020-07-28 17:29:02 +0200

    hacky option to provide custom where part and some split option

--- src/main/java/com/tenforce/etms/Main.java
+++ src/main/java/com/tenforce/etms/Main.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) throws RepositoryException, RDFHandlerException, FileNotFoundException {
@@ -36,7 +37,13 @@ public class Main {
     long offset = 0;
     if (null != System.getenv("SPARQL_OFFSET")) {
       offset = new Long(System.getenv("SPARQL_OFFSET"));
-    }   
+    }
+
+    boolean splitFiles = false;
+    if (null != System.getenv("SPLIT_FILES")) {
+      splitFiles = true;
+    }
+
 
     if ((null == graph && null==sparqlWherePart) || null == sparqlEndpoint) {
       logger.error("SPARQL_ENDPOINT and/or SPARQL_GRAPH or SPARQL_WHERE ENV variable are not set");
@@ -58,34 +65,50 @@ public class Main {
 
     try {
       URI applicationGraph = ValueFactoryImpl.getInstance().createURI(graph);
-      TupleQueryResult result = con.prepareTupleQuery(QueryLanguage.SPARQL, "SELECT (COUNT(*) as ?count) WHERE {GRAPH <" + applicationGraph.toString() + "> { ?s ?p ?o }}").evaluate();
+      String wherePart = "GRAPH <" + applicationGraph.toString() + "> { ?s ?p ?o }";
+      if (null!=sparqlWherePart && !sparqlWherePart.isEmpty())
+          wherePart = sparqlWherePart;
+      String countQuery =  "SELECT (COUNT(*) as ?count) WHERE {"+wherePart+"}";
+      //System.out.println(countQuery);
+      TupleQueryResult result = con.prepareTupleQuery(QueryLanguage.SPARQL, countQuery).evaluate();
       long amount = new Long(result.next().getBinding("count").getValue().stringValue());
       logger.info("dumping " + amount + " triples in batches of " + batchSize);
+      String filename = "sparql-dump.nt";
+      FileOutputStream stream = new FileOutputStream(new File(theDir.getPath(), filename));
+      RDFWriter writer = Rio.createWriter(RDFFormat.NTRIPLES, stream);
+      int iterations = 0;
       do {
         try {
-          String filename = "etms-" + offset + ".ttl";
-          FileOutputStream stream = new FileOutputStream(new File(theDir.getPath(), filename));
-          RDFWriter writer = Rio.createWriter(RDFFormat.NTRIPLES, stream);
+          if (splitFiles){
+            filename = "sparql-dump-" + offset + ".nt";
+            stream = new FileOutputStream(new File(theDir.getPath(), filename));
+            writer = Rio.createWriter(RDFFormat.NTRIPLES, stream);
+          }
+
           String query = "CONSTRUCT {?s ?p ?o}" +
               "WHERE { " +
-              "GRAPH <" + applicationGraph.toString() + "> {" +
-              "?s ?p ?o. " +
-              "}}" +
+               sparqlWherePart +
+              "}" +
               " LIMIT " + batchSize + " OFFSET " + offset;
           con.prepareGraphQuery(
               QueryLanguage.SPARQL, query).evaluate(writer);
           offset = offset + batchSize;
-          logger.info("wrote  " + batchSize + " triples to " + filename);
-          stream.close();
+          iterations++;
+          logger.info("progress "+iterations*batchSize/Double.valueOf(amount) *100+ "% -  wrote  " + batchSize +  " triples to " + filename);
+          if (splitFiles)
+            stream.close();
         } catch (Exception e) {
           logger.warn("failed to retrieve triples from offset" + offset + " with batchsize " + batchSize);
           logger.warn(e.getMessage());
           logger.warn(String.valueOf(e.getCause()));
+          logger.warn(Arrays.toString(e.getStackTrace()));
         }
       } while (offset < amount);
+      stream.close();
     } catch (Exception e) {
       logger.warn(e.getMessage());
       logger.warn(String.valueOf(e.getCause()));
+      logger.warn(Arrays.toString(e.getStackTrace()));
     } finally {
       con.close();
     }
