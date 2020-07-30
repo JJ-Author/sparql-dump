07356e99d5257ef4cf167e7b8081f73fd7de922b
JJ-Author <JJ-Author@users.noreply.github.com>
2020-07-30 22:28:52 +0200

    added jena sparq-api for complex construct queries

--- src/main/java/com/tenforce/etms/Main.java
+++ src/main/java/com/tenforce/etms/Main.java
@@ -1,5 +1,13 @@
 package com.tenforce.etms;
 
+import org.aksw.jena_sparql_api.cache.file.CacheBackendFile;
+import org.aksw.jena_sparql_api.core.FluentQueryExecutionFactory;
+import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
+import org.aksw.jena_sparql_api.rx.RDFDataMgrEx;
+import org.aksw.jena_sparql_api.rx.RDFDataMgrRx;
+import org.aksw.jena_sparql_api.stmt.SparqlQueryParserImpl;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.riot.RDFDataMgr;
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.ValueFactoryImpl;
 import org.openrdf.query.QueryLanguage;
@@ -18,6 +26,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
   public static void main(String[] args) throws RepositoryException, RDFHandlerException, FileNotFoundException {
@@ -28,6 +37,7 @@ public class Main {
     String user = System.getenv("SPARQL_USER");
     String password = System.getenv("SPARQL_PASSWORD");
     String sparqlWherePart = System.getenv("SPARQL_WHERE");
+    String constructQuery = System.getenv("SPARQL_CONSTRUCT");
 
     long batchSize = 50000;
     if (null != System.getenv("SPARQL_BATCHSIZE")) {
@@ -44,10 +54,15 @@ public class Main {
       splitFiles = true;
     }
 
+    String fileNamePrefix = "sparql-dump";
+    if (null != System.getenv("FILE_NAME_PREFIX") && ! System.getenv("FILE_NAME_PREFIX").isEmpty()) {
+      fileNamePrefix = System.getenv("FILE_NAME_PREFIX");
+    }
 
-    if ((null == graph && null==sparqlWherePart) || null == sparqlEndpoint) {
-      logger.error("SPARQL_ENDPOINT and/or SPARQL_GRAPH or SPARQL_WHERE ENV variable are not set");
-      throw new RuntimeException("SPARQL_ENDPOINT and/or SPARQL_GRAPH or SPARQL_WHERE ENV variable are not set");
+
+    if ((null == graph && null==sparqlWherePart && null == constructQuery) || null == sparqlEndpoint ) {
+      logger.error("SPARQL_ENDPOINT and/or SPARQL_GRAPH or SPARQL_WHERE or SPARQL_CONSTRUCT ENV variable are not set");
+      throw new RuntimeException("SPARQL_ENDPOINT and/or SPARQL_GRAPH or SPARQL_WHERE or SPARQL_CONSTRUCT ENV variable are not set");
     }
 
     File theDir = new File("dumps");
@@ -55,7 +70,13 @@ public class Main {
       theDir.mkdir();
     }
 
-    SPARQLRepository repo = new SPARQLRepository(sparqlEndpoint);
+    if (null != constructQuery) {
+      dumpWithCustomContructQuery(sparqlEndpoint, constructQuery, batchSize, fileNamePrefix, theDir);
+      return;
+    }
+
+
+      SPARQLRepository repo = new SPARQLRepository(sparqlEndpoint);
     if (null != user && null != password) {
       logger.debug("using authentication");
       repo.setUsernameAndPassword(user, password);
@@ -64,8 +85,11 @@ public class Main {
     RepositoryConnection con = repo.getConnection();
 
     try {
-      URI applicationGraph = ValueFactoryImpl.getInstance().createURI(graph);
-      String wherePart = "GRAPH <" + applicationGraph.toString() + "> { ?s ?p ?o }";
+      String wherePart = "?s ?p ?o";
+      if (null != graph) {
+        URI applicationGraph = ValueFactoryImpl.getInstance().createURI(graph);
+        wherePart = "GRAPH <" + applicationGraph.toString() + "> { ?s ?p ?o }";
+      }
       if (null!=sparqlWherePart && !sparqlWherePart.isEmpty())
           wherePart = sparqlWherePart;
       String countQuery =  "SELECT (COUNT(*) as ?count) WHERE {"+wherePart+"}";
@@ -73,14 +97,14 @@ public class Main {
       TupleQueryResult result = con.prepareTupleQuery(QueryLanguage.SPARQL, countQuery).evaluate();
       long amount = new Long(result.next().getBinding("count").getValue().stringValue());
       logger.info("dumping " + amount + " triples in batches of " + batchSize);
-      String filename = "sparql-dump.nt";
+      String filename = fileNamePrefix+".nt";
       FileOutputStream stream = new FileOutputStream(new File(theDir.getPath(), filename));
       RDFWriter writer = Rio.createWriter(RDFFormat.NTRIPLES, stream);
       int iterations = 0;
       do {
         try {
           if (splitFiles){
-            filename = "sparql-dump-" + offset + ".nt";
+            filename = fileNamePrefix + offset + ".nt";
             stream = new FileOutputStream(new File(theDir.getPath(), filename));
             writer = Rio.createWriter(RDFFormat.NTRIPLES, stream);
           }
@@ -112,5 +136,29 @@ public class Main {
     } finally {
       con.close();
     }
+  }
+
+  public static void dumpWithCustomContructQuery(String sparqlEndpoint, String constructQuery, long batchSize, String fileName, File theDir) {
+    QueryExecutionFactory qef = FluentQueryExecutionFactory.http(sparqlEndpoint)
+            .config()
+            .withCache(new CacheBackendFile(new File("/tmp/cache/foo"), 60 * 60 * 1000 /* ms */)) // Das ist der letzte Wrapper in der Chain
+            .withPagination((int) batchSize)
+            .withClientSideConstruct()
+            .withParser(SparqlQueryParserImpl.create()) // Der wrapper kommt zuerst
+            .end()
+            .create();
+    Iterator<Triple> rs = qef.createQueryExecution(constructQuery).execConstructTriples();
+    FileOutputStream stream = null;
+    try {
+      stream = new FileOutputStream(new File(theDir.getPath(), fileName+".nt"));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    RDFDataMgr.writeTriples(stream, rs);
+//    while(rs.hasNext()) {
+//      Triple t = rs.next();
+//
+//
+//    }
   }
 }
